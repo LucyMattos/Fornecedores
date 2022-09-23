805f05fe5e999ee4179c9fd0c3cc859d7759f0ca
Lucy Mattos <lucymattos32@hotmail.com>
2022-09-23 15:19:45 -0300

    Setup initial

--- fornecedores/src/app/app.component.ts
+++ fornecedores/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fornecedores';
+  form!: FormGroup;
+
+constructor(  private fb: FormBuilder,) {
+  this.form = this.fb.group({
+    id: [null],
+    name: [null],
+    lastName: [null],
+    nationality: [null],
+    zipCode: [null],
+    state: [null],
+    city: [null],
+    address: [null],
+    email: [null],
+    phone: [null],
+    federalId: [null],
+  });
+
+}
+  
+  get formControls() {
+    return this.form.controls;
+}
+
 }
--- fornecedores/src/app/app.module.ts
+++ fornecedores/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { AppMaterialModule } from 'src/shared/material.module';
+import { FormGroupDirective, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    AppMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [FormGroupDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
